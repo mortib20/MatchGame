5b2490930af1bcb02b10d262123cfc62b3b79629
mortib20 <martin.paul-3277@web.de>
2023-09-02 12:13:00 +0200

    Projektdateien hinzufügen.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            List<string> animalEmojis = new()
+            {
+                "🐵", "🐵",
+                "🦍", "🦍",
+                "🐶", "🐶",
+                "🐕", "🐕",
+                "🐩", "🐩",
+                "🐺", "🐺",
+                "🦊", "🦊",
+                "🐱", "🐱",
+            };
+
+            Random random = new();
+
+            foreach (var textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmojis.Count);
+                string nextEmoji = animalEmojis[index];
+                textBlock.Text = nextEmoji;
+                animalEmojis.RemoveAt(index);
+            }
+        }
+    }
+}
